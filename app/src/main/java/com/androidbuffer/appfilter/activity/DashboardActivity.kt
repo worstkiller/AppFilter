eecb12bdbb358e7cbb1b54c9ce4c600f39cf03b4
worstkiller <vkbookworm@gmail.com>
2018-07-10 00:03:56 +0530

    feat: Added some drawables

---
+++ app/src/main/java/com/androidbuffer/appfilter/activity/DashboardActivity.kt
@@ -0,0 +1,17 @@
+package com.androidbuffer.appfilter.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.androidbuffer.appfilter.R
+import kotlinx.android.synthetic.main.activity_dashboard.view.*
+
+/**
+ * Created by incred on 9/7/18.
+ */
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
